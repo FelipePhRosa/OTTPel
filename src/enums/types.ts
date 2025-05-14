9a76eb8758721e85d750e16ca3df3250b2d31a17
FelipePhRosa <ofcFelipeRosa@gmail.com>
2025-05-13 22:13:52 -0300

    Basic enums roles.

---
+++ src/enums/types.ts
@@ -0,0 +1,14 @@
+enum Reportstatus {
+    PENDENTE = 'pendente',
+    APROVADO = 'aprovado',
+    REJEITADO = 'rejeitado'
+};
+
+enum Role {
+    Owner = '1',
+    Admin = '2',
+    Moderador = '3',
+    Suporte = '4',
+    UserComum = '5',
+    Banned = '6'
+};
