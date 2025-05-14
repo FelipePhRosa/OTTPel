958f8b94ae7451f9921b28f482ee0042aec40a53
FelipePhRosa <ofcFelipeRosa@gmail.com>
2025-05-13 22:08:47 -0300

    Server Default Config.

---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import express from "express"
+import dotenv from "dotenv"
+import cors from "cors"
+import router from "./router"
+
+dotenv.config()
+
+const server = express()
+const PORT = 3000
+
+server.use(cors());
+
+server.use(express.json());
+server.use(router)
+
+server.listen(PORT, () => console.log(`Server Listening: ${PORT}`));
